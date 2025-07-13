34387188c565d5a703f74ae147964b9a57e9aa3b
Masafumi Kaga <mteszk@gmail.com>
2025-07-13 21:31:29 +0900

    fix import

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,6 @@
 // sum.test.ts
-import { expect, test } from "bun";
-import { sum } from "./sum";
+import { expect, test } from "bun:test";
+import { sum } from "./index";
 
 test("adds two numbers", () => {
   expect(sum(1, 2)).toBe(3);
