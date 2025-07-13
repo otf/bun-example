08cb99e868b0cd62d14c1e8e82fd8563a225b68d
Masafumi Kaga <mteszk@gmail.com>
2025-07-14 08:25:58 +0900

    format

--- flake.nix
+++ flake.nix
@@ -4,11 +4,23 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit self;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             bun
