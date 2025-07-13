487eba38dcb32cb7238638258d0f7886f7248a54
Masafumi Kaga <mteszk@gmail.com>
2025-07-13 22:21:43 +0900

    effect succeed

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,13 @@
 // sum.test.ts
 import { expect, test } from "bun:test";
 import { sum } from "./index";
+import { Effect } from "effect"
 
 test("adds two numbers", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+
+test("success", () => {
+  expect(Effect.runSync(Effect.succeed(42))).toBe((42));
+});
